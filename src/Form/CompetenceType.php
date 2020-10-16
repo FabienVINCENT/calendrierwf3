5f5f743f5ec2fd6b38df15a71bbd195ef8dd98d9
10-06 <69347639+10-06@users.noreply.github.com>
2020-10-16 14:14:18 +0200

    Modif styles

--- src/Form/CompetenceType.php
+++ src/Form/CompetenceType.php
@@ -6,13 +6,15 @@ use App\Entity\Competence;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CompetenceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
+        ->add('nom', TextType::class, ['label' => 'Nom du formateur', 'attr' => ['placeholder' => 'Champ obligatoire'], ])
         ;
     }
 
