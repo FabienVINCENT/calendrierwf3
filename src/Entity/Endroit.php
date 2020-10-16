12505a4defd859832a739c12f36fe51624750acd
aurelien briard <aurelienbriard@gmail.com>
2020-10-16 09:15:57 +0200

    template_endroit
    mise en place de la protection du champ pour saisir un endroit
    + un peu de déco sur le template endroit

--- src/Entity/Endroit.php
+++ src/Entity/Endroit.php
@@ -6,6 +6,8 @@ use App\Repository\EndroitRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=EndroitRepository::class)
@@ -20,7 +22,12 @@ class Endroit
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=60)
+     * @ORM\Column(type="string", length=60, unique=true)
+     * @Assert\NotBlank (message = "Champ obligatoire")
+     * @Assert\Length(min = 5, max = 60,
+     *      minMessage = "Veuillez entrer 5 caractère au minimum",
+     *      maxMessage = "Veuillez entrer 60 caractère au maximum"
+     * )
      */
     private $ville;
 
