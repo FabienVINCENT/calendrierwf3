d14fdcd01dd320fc90ea84841cb3becb171c1d81
Damien <69347370+Dramien@users.noreply.github.com>
2020-10-19 15:42:58 +0200

    AjoutModaleDate

--- public/assets/js/calendrier.js
+++ public/assets/js/calendrier.js
@@ -14,7 +14,16 @@ window.onload = () =>
 		},
 		editable: true,
 		eventResizableFromStart: true,
-		selectable: true
+		selectable: true,
+		dateClick : function(info) {
+
+			const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+			let dateObjet= new Date (info.dateStr);
+
+				$('#modalAjoutDate').modal('show');
+				$('#modalAjoutDateTitle').html('Enregistrement pour le ' + dateObjet.toLocaleDateString('fr-FR',options));
+			}
 	})
 	
 	calendar.render()
