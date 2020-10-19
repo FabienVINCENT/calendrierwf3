9437d869f042843220c984a20969d1a3161b1df7
Damien <69347370+Dramien@users.noreply.github.com>
2020-10-19 17:07:39 +0200

    mep

--- public/assets/js/calendrier.js
+++ public/assets/js/calendrier.js
@@ -15,6 +15,14 @@ window.onload = () =>
 		editable: true,
 		eventResizableFromStart: true,
 		selectable: true,
+		weekends: false,
+		weekNumbers: true,
+		businessHours: {
+		  // days of week. an array of zero-based day of week integers (0=Sunday)
+		  daysOfWeek: [ 1, 2, 3, 4, 5 ], // Monday - Thursday
+		  startTime: '08:00', // a start time (10am in this example)
+		  endTime: '18:00', // an end time (6pm in this example)
+		},
 		dateClick : function(info) {
 
 			const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
